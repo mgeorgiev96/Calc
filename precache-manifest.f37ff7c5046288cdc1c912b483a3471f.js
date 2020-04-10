2997fe193a7624364273ae0230013316ac843f40
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-10 08:11:22 +0100

    Updates

---
+++ precache-manifest.f37ff7c5046288cdc1c912b483a3471f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a505c11c31ab61e9103699cca8ae7d21",
+    "url": "/Calc/index.html"
+  },
+  {
+    "revision": "08dc25fde3e5585f53b5",
+    "url": "/Calc/static/css/main.107d95cb.chunk.css"
+  },
+  {
+    "revision": "4ad3d0eb5a2c018e7c03",
+    "url": "/Calc/static/js/2.f0cbc246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calc/static/js/2.f0cbc246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dc25fde3e5585f53b5",
+    "url": "/Calc/static/js/main.fcb79b51.chunk.js"
+  },
+  {
+    "revision": "e08a838bcf934cace06f",
+    "url": "/Calc/static/js/runtime-main.2eb668c9.js"
+  }
+]);
--- src/Buttons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Buttons(props) {
-    return (
-       <p className={props.value}>{props.id}</p>
-    );
-}
-
-export default Buttons;
